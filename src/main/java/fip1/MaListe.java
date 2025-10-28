41c5eb8558ad8269e47e81f92a0f75739a22a81e
Thomas Grouazel <grouazel.thomas@gmail.com>
2025-10-28 15:34:56 +0100

    Tp termine

--- src/main/java/fip1/MaListe.java
+++ src/main/java/fip1/MaListe.java
@@ -1,7 +1,55 @@
 package fip1;
 
-import java.util.Arrays;
+public class MaListe<T> {
+    private Object[] elements;
+    private int size;
 
-public class MaListe {
+    public MaListe() {
+        this.elements = new Object[10]; 
+        this.size = 0;
+    }
+
+    // Ajoute un élément en fin de liste
+    public void ajouter(T element) {
+        elements[size] = element;
+        size++;
+    }
+
     
+    public int taille() {
+        return size;
+    }
+
+    // R à indice i
+    @SuppressWarnings("unchecked")
+    public T element(int i) {
+        return (T) elements[i];
+    }
+
+    // ajoute el d'une liste dans la liste courante
+    public void ajouterListe(MaListe<? extends T> autreListe) {
+        for (int i = 0; i < autreListe.taille(); i++) {
+            this.ajouter(autreListe.element(i));
+        }
+    }
+
+    // inverse, ajoute les el de liste courante dans autreListe
+    public void ajouterDansListe(MaListe<? super T> autreListe) {
+        for (int i = 0; i < this.taille(); i++) {
+            autreListe.ajouter(this.element(i));
+        }
+    }
+
+    // stringify les el de la liste
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            sb.append(elements[i]);
+            if (i < size - 1) {
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
 }
--- src/main/java/fip1/MaListeHelper.java
+++ src/main/java/fip1/MaListeHelper.java
@@ -1,8 +1,15 @@
 package fip1;
 
-/**
- * Une collection de méthodes statiques liées à la classe "MaListe"
- */
 public class MaListeHelper {
-    
+
+    public static <T> MaListe<T> concat(MaListe<? extends T> l1, MaListe<? extends T> l2) {
+        MaListe<T> result = new MaListe<>();
+        for (int i = 0; i < l1.taille(); i++) {
+            result.ajouter(l1.element(i));
+        }
+        for (int i = 0; i < l2.taille(); i++) {
+            result.ajouter(l2.element(i));
+        }
+        return result;
+    }
 }
